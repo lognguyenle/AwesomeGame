c714560ef1ab9d045e9e086f339274904784df0f
lognguyenle <lolopthyrealone@gmail.com>
2022-09-14 13:56:15 -0700

    Improved VisualNovel Choice generator

--- test/core/src/com/mygdx/game/MyGdxGame.java
+++ test/core/src/com/mygdx/game/MyGdxGame.java
@@ -4,9 +4,6 @@ import org.w3c.dom.Text;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.loaders.SkinLoader.SkinParameter;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.utils.Align;
@@ -27,10 +24,14 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.Timer.Task;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.math.*;
 
 public class MyGdxGame extends ApplicationAdapter {
 
@@ -38,13 +39,21 @@ public class MyGdxGame extends ApplicationAdapter {
 	private Group group;
 	private Table table;
 
+	// copied code from http://acamara.es/blog/2012/02/keep-screen-aspect-ratio-with-different-resolutions-using-libgdx/
+	private static final int VIRTUAL_WIDTH = 1920;
+    private static final int VIRTUAL_HEIGHT = 1080;
+    private static final float ASPECT_RATIO =
+        (float)VIRTUAL_WIDTH/(float)VIRTUAL_HEIGHT;
+    private Camera camera;
+    private Rectangle viewport;
+
 	//DialogueMarker Actor
 	public class DialogueMarker extends Actor {
 		Texture texture = new Texture(Gdx.files.internal("downarrow.png"));
 		@Override
 		public void draw(Batch batch, float alpha){
 			batch.setColor(this.getColor());
-			batch.draw(texture, 1300, 320);
+			batch.draw(texture, 1310, 320);
 			batch.setColor(batch.getColor());
 		}
 		@Override
@@ -52,11 +61,42 @@ public class MyGdxGame extends ApplicationAdapter {
 			super.act(delta);
 		}
 	}
+
+	//Gui pad calculator, variables have been added for easier understanding
+	public static int ChoiceGUICalc(String Type, int ChoiceAmount){
+		int result = 0;
+		int ChoiceHeight = 121;
+		int DialogueBoxPad = 508;
+		int VisualNovelHeight = 760;
+		int TextBoxHeight = 252;
+		int ChoiceBump = ((VisualNovelHeight-TextBoxHeight) - (ChoiceHeight*ChoiceAmount)) / (ChoiceAmount+1);
+		int ChoiceAll = ChoiceBump + ChoiceHeight;
+		
+		switch(Type) {
+			case "DialogueBox":
+			result = DialogueBoxPad - (ChoiceAmount*ChoiceAll);
+			break;
+
+			case "ChoiceBox":
+
+			break;
+
+			case "ChoiceBump":
+			result = ChoiceBump;
+			break;
+		}
+
+		return result;
+	} 
 	
 	
 	//Start of ApplicationListener life-cycle
 	@Override
 	public void create() {
+
+		
+		// copied code from http://acamara.es/blog/2012/02/keep-screen-aspect-ratio-with-different-resolutions-using-libgdx/
+		camera = new OrthographicCamera(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
 
 		//Creates stage and then respective actors
 		stage = new Stage();
@@ -67,33 +107,39 @@ public class MyGdxGame extends ApplicationAdapter {
 		table.setFillParent(true);
 		table.top().left();
 
-		Table VisualNovelTable = new Table();
-
-		Table DialogueBoxTable = new Table();
-
-		Table DialogueTextTable = new Table();
+		
 
 		//Setup tables for ui
-
-		Pixmap visualNovelBG = new Pixmap(Gdx.files.internal("background1.jpg"));
+		Table VisualNovelTable = new Table();
+		Pixmap visualNovelBG = new Pixmap(Gdx.files.internal("background2.jpg"));
 		TextureRegionDrawable visualNovelBGDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(visualNovelBG)));
 		VisualNovelTable.setBackground(visualNovelBGDrawable);
 		table.add(VisualNovelTable).padLeft(20).padTop(20);
 
+		Table Choices1 = new Table();
+		Pixmap ChoiceBG = new Pixmap(Gdx.files.internal("choicebox 3.png"));
+		TextureRegionDrawable ChoiceBGDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(ChoiceBG)));
+		Choices1.setBackground(ChoiceBGDrawable);
+		Choices1.setTouchable(Touchable.enabled);
+
+		Table Choices2 = new Table();
+		Choices2.setBackground(ChoiceBGDrawable);
+		Choices2.setTouchable(Touchable.enabled);
+
+		VisualNovelTable.setDebug(true);
+		Table DialogueBoxTable = new Table();
 		Pixmap DialogueBG = new Pixmap(Gdx.files.internal("dialogue box.png"));
 		TextureRegionDrawable DialogueBGDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(DialogueBG)));
 		DialogueBoxTable.setBackground(DialogueBGDrawable);
 		DialogueBoxTable.setTouchable(Touchable.enabled);
-		VisualNovelTable.add(DialogueBoxTable).padTop(508);
-		VisualNovelTable.left().top();
 
 		//UI DialogueLabel Widget
+		Table DialogueTextTable = new Table();
 		Label.LabelStyle TextDialogueLabelStyle = new Label.LabelStyle();
 		BitmapFont defaultFont = new BitmapFont(Gdx.files.internal("font.fnt"));
 		TextDialogueLabelStyle.font = defaultFont;
 		TextDialogueLabelStyle.fontColor = Color.WHITE;
 		final Label DialogueLabel = new Label("", TextDialogueLabelStyle);
-
 		DialogueLabel.setAlignment(Align.left);
 		DialogueLabel.setWrap(true); 
 		DialogueLabel.setTouchable(Touchable.disabled);
@@ -103,12 +149,6 @@ public class MyGdxGame extends ApplicationAdapter {
 		scrolly.setScrollbarsVisible(false);
 		scrolly.setScrollY(20);
 		scrolly.setTouchable(Touchable.disabled);
-	
-		DialogueTextTable.add(scrolly).width(1250);
-		DialogueTextTable.setZIndex(6);
-		DialogueTextTable.left().top();
-		DialogueTextTable.setTouchable(Touchable.disabled);
-		DialogueBoxTable.add(DialogueTextTable).width(1250).height(188);
 
 		//Intialize DialogueMarker actor
 		final DialogueMarker dialogueMarker = new DialogueMarker();
@@ -137,8 +177,27 @@ public class MyGdxGame extends ApplicationAdapter {
 		dialogueMarkerLooper.setAction(dialogueMarkerLoop);
 		dialogueMarker.addAction(dialogueMarkerLooper);
 
+		//Adding Texttable 
+		DialogueTextTable.add(scrolly).width(1250);
+		DialogueTextTable.setZIndex(6);
+		DialogueTextTable.left().top();
+		DialogueTextTable.setTouchable(Touchable.disabled);
+		DialogueBoxTable.add(DialogueTextTable).width(1250).height(188 - Choices1.getHeight());
+
+
 		
-		//Typewriter effect for clicking DialogueBoxTable
+		//Adding Choice buttons, will move later, will probably add to type writer effect below.
+		int Choices = 2;
+		int ChoiceBump = ChoiceGUICalc("ChoiceBump", Choices);
+		VisualNovelTable.add(Choices1).padTop(ChoiceBump);
+		VisualNovelTable.row();
+		VisualNovelTable.add(Choices1).padTop(ChoiceBump);
+		VisualNovelTable.row();
+		VisualNovelTable.add(DialogueBoxTable).padTop(ChoiceGUICalc("DialogueBox", Choices));
+		VisualNovelTable.left().top();
+
+
+		//Typewriter effect for clicking DialogueBoxTable, will add script scanner and move choice gui generator to stuff below.
 		DialogueBoxTable.addListener(new ClickListener(){
 			@Override
 			public void clicked(InputEvent event, float x, float y){
@@ -161,17 +220,52 @@ public class MyGdxGame extends ApplicationAdapter {
 							System.out.println("done?");
 			}});
 		
+		
+
+
+
+
 	}
 		
 	@Override
 	public void render () {
+		// update camera,  copied code from http://acamara.es/blog/2012/02/keep-screen-aspect-ratio-with-different-resolutions-using-libgdx/
+        camera.update();
+        // camera.apply,  copied code from http://acamara.es/blog/2012/02/keep-screen-aspect-ratio-with-different-resolutions-using-libgdx/
+		Gdx.gl.glViewport((int) viewport.x, (int) viewport.y,
+                          (int) viewport.width, (int) viewport.height);
+		// clear previous frame
 		ScreenUtils.clear(0, 0, 0, 1);
 		stage.act();
 		stage.draw();
 		
 		
 	}
-	
+	@Override
+    public void resize(int width, int height){
+        // calculate new viewport, copied code from http://acamara.es/blog/2012/02/keep-screen-aspect-ratio-with-different-resolutions-using-libgdx/
+        float aspectRatio = (float)width/(float)height;
+        float scale = 1f;
+        Vector2 crop = new Vector2(0f, 0f);
+		if(aspectRatio > ASPECT_RATIO)
+        {
+            scale = (float)height/(float)VIRTUAL_HEIGHT;
+            crop.x = (width - VIRTUAL_WIDTH*scale)/2f;
+        }
+        else if(aspectRatio < ASPECT_RATIO)
+        {
+            scale = (float)width/(float)VIRTUAL_WIDTH;
+            crop.y = (height - VIRTUAL_HEIGHT*scale)/2f;
+        }
+        else
+        {
+            scale = (float)width/(float)VIRTUAL_WIDTH;
+        }
+
+        float w = (float)VIRTUAL_WIDTH*scale;
+        float h = (float)VIRTUAL_HEIGHT*scale;
+        viewport = new Rectangle(crop.x, crop.y, w, h);
+    }
 	@Override
 	public void dispose () {
 		stage.dispose();
