70f2e65aace1b8d0e56e09c78498d5ceda6bc325
lognguyenle <lolopthyrealone@gmail.com>
2023-02-10 23:55:38 -0800

    Fixing files

---
+++ AwesomeGame/html/src/com/awesomegame/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.awesomegame.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.awesomegame.game.AwesomeGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new AwesomeGame();
+        }
+}
