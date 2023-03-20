28712984356334f71c2761922884e26d665cc82f
lognguyenle <lolopthyrealone@gmail.com>
2023-03-20 14:25:26 -0700

    delete

--- mainProject/core/bin/main/resources/generation/cleanup.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-# data: https://github.com/plotly/datasets/blob/master/us-cities-top-1k.csv
-df = pd.read_csv('./cities.csv')
-df = df.sort_values('State')
-df.to_csv('data.csv', index=False, header=False)
-print(df['City'][0])
